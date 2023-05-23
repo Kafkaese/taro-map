257dfbefdc06dca4e9da4f8242530494234590b4
Frederik Laubisch <kafkaese@gmail.com>
2023-05-23 09:53:28 +0400

    add man app file

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WorldMap from './WorldMap';
+
+const App = () => {
+  return (
+    <div>
+      <h1>World Map</h1>
+      <WorldMap />
+    </div>
+  );
+};
+
+export default App;
