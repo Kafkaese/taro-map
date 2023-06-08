8b3a46fa2821aa1e785d5efbe9accb7a2bc0bc27
Frederik Laubisch <kafkaese@gmail.com>
2023-06-08 18:32:25 +0200

    fix slider box text adjustement

--- src/components/YearSlider.js
+++ src/components/YearSlider.js
@@ -14,8 +14,8 @@ const YearSlider = ({ onYearChange }) => {
     return (
       <div>
         <div className='slider-year-info-box'>
-          <div className='slider-year-info-box-elements'>Select year: </div>
-          <div className='slider-year-info-box-elements'>{year}</div>  
+          <div className='slider-year-info-box-element1'>Select year: </div>
+          <div className='slider-year-info-box-element2'>{year}</div>  
         </div>
         <ReactSlider
             className="horizontal-slider"
