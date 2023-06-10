a22fa390bf2fbad574c1794dee10dd9b780e2cb3
Frederik Laubisch <kafkaese@gmail.com>
2023-06-10 18:58:24 +0200

    add very simple custom toggle button

---
+++ src/components/ToggleButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './ToggleButton.css'
+const ToggleButton = () => {
+
+
+    return (
+        <div className="wrapper">
+            <div>Export</div>
+            <div>Import</div>
+        </div>
+    );
+}
+
+export default ToggleButton;
