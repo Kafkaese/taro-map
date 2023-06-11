c4a341eb87d418cd767d8edc83b4fa3bc922abbc
Frederik Laubisch <kafkaese@gmail.com>
2023-06-11 15:10:07 +0200

    generalize button

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useState } from "react";
+
 import './ToggleButton.css'
-const ToggleButton = () => {
+
+const ToggleButton = ({left, right}) => {
+
 
 
     return (
         <div className="wrapper">
-            <div className="text-box"><text className="text">Export</text></div>
-            <div className="text-box"><text className="text">Import</text></div>
+            <div className="text-box"><text className="text">{left}</text></div>
+            <div className="text-box"><text className="text">{right}</text></div>
         </div>
     );
 }
