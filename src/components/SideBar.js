9148df85eceb24b41d3fa0954df481b5a6be4dc9
Frederik Laubisch <kafkaese@gmail.com>
2023-06-13 15:13:57 +0200

    sidebar collapseable

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import './SideBar.css'
 
 const SideBar = () => {
 
+    const [collapsed, setCollapsed] = useState(true)
     return (
-        <div className="sideBar">
+        <div className="sideBar" style={collapsed ? {width: '0%'} : {width: '30%'}}>
 
         </div>
     )
