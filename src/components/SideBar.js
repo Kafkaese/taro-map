442010d02fd4886d622f7d16b7ebfc3a38a7041e
Frederik Laubisch <kafkaese@gmail.com>
2023-06-13 15:21:57 +0200

    add (empty) collapse button to sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,10 +3,13 @@ import './SideBar.css'
 
 const SideBar = () => {
 
-    const [collapsed, setCollapsed] = useState(true)
+    const [collapsed, setCollapsed] = useState(false)
     return (
-        <div className="sideBar" style={collapsed ? {width: '0%'} : {width: '30%'}}>
+        <div className="sideBar">
+            <div className="panel" style={collapsed ? {width: '0%'} : {width: '30%'}}>
 
+            </div>
+            <button className="button" style={collapsed ? {width: '0%'} : {left: '30.45%'}}></button>
         </div>
     )
 }
