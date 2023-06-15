521563f8326da8e293ba2566e53420635743e791
Frederik Laubisch <kafkaese@gmail.com>
2023-06-15 09:30:58 +0200

    refactor import map

--- src/components/ImportMap.js
+++ src/components/ImportMap.js
@@ -8,33 +8,70 @@ import './HoverBox.css';
 
 const ImportMap = ({year, zoom, onCountryChange}) => {
 
-  // map defaulo colors
+  // geometry colors
   const defaultColor = '#84B098';
   const hoverColor = '#66B087';
-  
-  // Data fot sidebar
-  const [activeCountryData, setActiveCountryData] = useState({});
 
-  // Map states
-  //const [zoom, setZoom] = useState(1);
+  // Hover states
   const [hoveredCountry, setHoveredCountry] = useState(null);
-  const [countryData, setCountryData] = useState({});
+  const [hoveredCountryData, setHoveredCountryData] = useState({});
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  // Track mouse and let tooltip follow
+  const handleMouseMove = (event) => {
+
+    const { clientX, clientY } = event;
+
+    setMousePosition({ x: clientX, y: clientY });
+
+    if (hoveredCountry) {
+      setHoveredCountry({...hoveredCountry, position: mousePosition})
+    }
+
+  };
+
+  // Mouse enter  for hover tool
+  const handleMouseEnterBox = (event) => {
+    setHoveredCountry(null)
+  }
+  
+  // Tooltip data fetching 
+  const handleCountryHover = async (alpha2, name) => {
+    try {
+      const democracy_index = await fetch(`http://${HOST}:${API_PORT}/metadata/democracy_index?country_code=${alpha2}&year=${year}`);
+      const total_imports = await fetch(`http://${HOST}:${API_PORT}/imports/year?country_code=${alpha2}&year=${year}`);
+      const peace_index = await fetch(`http://${HOST}:${API_PORT}/metadata/peace_index?country_code=${alpha2}&year=${year}`);
+      
+      const democracy_index_data = await democracy_index.json();
+      const peace_index_data = await peace_index.json();
+      const total_imports_data = await total_imports.json();
+     
+      // Populate data for tooltip with API resonses
+      setHoveredCountryData({ democracy_index: democracy_index_data, peace_index: peace_index_data,total_imports: total_imports_data});
+
+      // Set hovered country state
+      setHoveredCountry({ name, position: mousePosition });
+
+    } catch (error) {
+      console.error('Error fetching country data:', error);
+    }
+  };
+
+  // Remove hover tool when leaving geometry
+  const handleCountryLeave = (event) => {
+    setHoveredCountry(null)
+    event.target.setAttribute('fill', defaultColor);
+  };
+
 
   // Collapse for sidebar
   const [collapsed, setCollapsed] = useState(false)
 
-  // Track mouse and hover tool follow
-  const handleMouseMove = (event) => {
-    const { clientX, clientY } = event;
-    setMousePosition({ x: clientX, y: clientY });
-    if (hoveredCountry) {
-      setHoveredCountry({...hoveredCountry, position: mousePosition})
-    }
-  };
-  
+  // Data for sidebar
+  const [activeCountryData, setActiveCountryData] = useState({});
+
   // Gets country data for sidebar from APIs
-  const handleCountryClick = async (alpha2, name, geography) => {
+  const handleCountryClick = async (alpha2) => {
 
     try {
 
@@ -53,46 +90,18 @@ const ImportMap = ({year, zoom, onCountryChange}) => {
       // update object with new data
       setActiveCountryData({ name: name_data, democracy_index: democracy_index_data, peace_index: peace_index_data, total_imports: total_imports_data, sources: sources_data});
 
+      // uncollpase sidebar if new country is selected
       setCollapsed(false)
 
     } catch (error) {
       console.error('Error fetching country data:', error);
     }
   };
-  // Mouse enter  for hover tool
-  const handleMouseEnterBox = (event) => {
-    setHoveredCountry(null)
-  }
 
-  // Actual hover tool logic with API calls
-  const handleCountryHover = async (alpha2, name, geography) => {
-    try {
-      const democracy_index = await fetch(`http://${HOST}:${API_PORT}/metadata/democracy_index?country_code=${alpha2}&year=${year}`);
-      const total_imports = await fetch(`http://${HOST}:${API_PORT}/imports/year?country_code=${alpha2}&year=${year}`);
-      const peace_index = await fetch(`http://${HOST}:${API_PORT}/metadata/peace_index?country_code=${alpha2}&year=${year}`);
-      
-      const democracy_index_data = await democracy_index.json();
-      const peace_index_data = await peace_index.json();
-      const total_imports_data = await total_imports.json();
-     
-      setCountryData({ democracy_index: democracy_index_data, peace_index: peace_index_data,total_imports: total_imports_data});
-
-      setHoveredCountry({ name, position: mousePosition });
-
-    } catch (error) {
-      console.error('Error fetching country data:', error);
-    }
-  };
-  
-  // Remove hover tool whne leaving geometry
-  const handleCountryLeave = (event) => {
-    setHoveredCountry(null)
-    event.target.setAttribute('fill', defaultColor);
-  };
 
   return (
     <div>
-      {typeof activeCountryData.name !== 'undefined' ? <SideBarImports countryData={activeCountryData} collapsed={collapsed} onCollapse={setCollapsed}></SideBarImports> : <div/>}
+      {typeof activeCountryData.name !== 'undefined' ? <SideBarImports hoveredCountryData={activeCountryData} collapsed={collapsed} onCollapse={setCollapsed}></SideBarImports> : <div/>}
       <ComposableMap
         projection="geoMercator"
         style={{ width: '100%', height: '93vh' }}
@@ -111,7 +120,7 @@ const ImportMap = ({year, zoom, onCountryChange}) => {
                     geography={geo}
                     onMouseOver={() => handleCountryHover(alpha2, name, geo)}
                     onMouseLeave={handleCountryLeave}
-                    onClick={() => handleCountryClick(alpha2, name, geo)}
+                    onClick={() => handleCountryClick(alpha2)}
                     style={{
                       default: {
                         fill: defaultColor,
@@ -148,23 +157,23 @@ const ImportMap = ({year, zoom, onCountryChange}) => {
         <div className="circle-container">
           
           <div className="money-wrapper">
-            <div className="money" style={{ backgroundColor: getUSDColor(countryData.total_imports.value) }}>
-              {formatUSDvalue(countryData.total_imports.value)}
+            <div className="money" style={{ backgroundColor: getUSDColor(hoveredCountryData.total_imports.value) }}>
+              {formatUSDvalue(hoveredCountryData.total_imports.value)}
             </div>
-            <div className='annotate'><div className='text'>{formatUSDorder(countryData.total_imports.value)}</div></div>
+            <div className='annotate'><div className='text'>{formatUSDorder(hoveredCountryData.total_imports.value)}</div></div>
             <span className='money-label'>Imports</span>
           </div>
 
           <div className='circle-wrapper'>
-            <div className="circle" style={{ backgroundColor: getDemocracyColor(countryData.democracy_index.value) }}>
-              {countryData.democracy_index.value}
+            <div className="circle" style={{ backgroundColor: getDemocracyColor(hoveredCountryData.democracy_index.value) }}>
+              {hoveredCountryData.democracy_index.value}
             </div>
             <span className='circle-label'>Democracy Index<sup>[1]</sup></span>
           </div>
 
           <div className='circle-wrapper'>
-            <div className="circle" style={{ backgroundColor: getPeaceColor(countryData.peace_index.value) }}>
-              {countryData.peace_index.value}
+            <div className="circle" style={{ backgroundColor: getPeaceColor(hoveredCountryData.peace_index.value) }}>
+              {hoveredCountryData.peace_index.value}
             </div>
             <span className='circle-label'>Peace Index <sup>[2]</sup></span>
           </div>
