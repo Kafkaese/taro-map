50970be7921f72defced1186d9b0869ca4c80e8f
Frederik Laubisch <kafkaese@gmail.com>
2023-07-31 15:02:31 +0200

    use state from triggering workflow

--- terraform/deploy/deploy.tf
+++ terraform/deploy/deploy.tf
@@ -1,21 +1,28 @@
-# resource group for all Azure resources
-resource "azurerm_resource_group" "rg" {
-  location = var.resource_group_location
-  name     = var.resource_group_name
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "first_configuration" {
+  backend = "azurerm"
+  config = {
+    storage_account_name = "taro"
+    container_name       = "terraform-staging-env"
+    key                  = "taging.tfstate"
+  }
 }
 
 # Container registry for the frontend container
 resource "azurerm_container_registry" "container-registry" {
   name                = var.acr_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.terraform_remote_state.first_configuration.outputs.rg-name
+  location            = data.terraform_remote_state.first_configuration.outputs.rg-location
   sku                 = "Basic"
 }
 
 # Container Instance for the frontend
 resource "azurerm_container_group" "container-instance" {
   name                = var.instance_name
-  location            = var.resource_group_location
+  location            = data.terraform_remote_state.first_configuration.outputs.rg-location
   resource_group_name = var.resource_group_name
   ip_address_type     = "Public"
   os_type             = "Linux"
