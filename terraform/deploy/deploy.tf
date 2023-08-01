c8addd25a04fe6a1a70ecc5b7038c18a71bea023
Frederik Laubisch <kafkaese@gmail.com>
2023-08-01 11:27:11 +0200

    change url path

--- terraform/deploy/deploy.tf
+++ terraform/deploy/deploy.tf
@@ -1,6 +1,6 @@
 # Use shared resource module from taro-tf
 module "shared-resources" {
-  source = "git::https://github.com/Kafkaese/taro-tf//staging/shared_resource_module?ref=staging"
+  source = "git::https://github.com/Kafkaese/taro-tf//staging_env/shared_resource_module?ref=staging"
 
   resource_group_name = var.resource_group_name
   resource_group_location = var.resource_group_location
