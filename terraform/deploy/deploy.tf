ce463019d6a75f9aae366ba5047b2c48d0134a06
Frederik Laubisch <kafkaese@gmail.com>
2023-08-01 13:43:03 +0200

    try ignoring vars

--- terraform/deploy/deploy.tf
+++ terraform/deploy/deploy.tf
@@ -2,9 +2,6 @@
 module "shared-resources" {
   source = "git::https://github.com/Kafkaese/taro-tf//staging_env/shared_resource_module?ref=staging_branch"
 
-  resource_group_name = var.resource_group_name
-  resource_group_location = var.resource_group_location
-  container_registry_name = var.acr_name
 }
 
 # Container Instance for the frontend
