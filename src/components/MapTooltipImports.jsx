cd72d2b7f2dd865eb773513d2004a5340df3e2bf
Frederik Laubisch <kafkaese@gmail.com>
2023-09-08 10:48:29 +0200

    render tooltip conditionally

--- src/components/MapTooltipImports.jsx
+++ src/components/MapTooltipImports.jsx
@@ -6,36 +6,40 @@ const MapTooltip = (hoveredCountry, handleMouseEnterBox) => {
     console.log(hoveredCountry)
     
     return (
-        <div className="hover-box-container" style={{top: hoveredCountry.position.y +5, left: hoveredCountry.position.x +10,}}
-            onMouseEnter={handleMouseEnterBox}>
-                <h3>{hoveredCountry.countryName.value}</h3>
+        <div>
+            {hoveredCountry.totalImports &&
+            <div className="hover-box-container" style={{top: hoveredCountry.position.y +5, left: hoveredCountry.position.x +10,}}
+                onMouseEnter={handleMouseEnterBox}>
+                    <h3>{hoveredCountry.countryName.value}</h3>
 
-                
-                <div className="circle-container">
-                
-                <div className="money-wrapper">
-                    <div className="money" style={{ backgroundColor: getUSDColor(hoveredCountry.totalImports.value) }}>
-                    {formatUSDvalue(hoveredCountry.totalImports.value)}
+                    
+                    <div className="circle-container">
+                    
+                    <div className="money-wrapper">
+                        <div className="money" style={{ backgroundColor: getUSDColor(hoveredCountry.totalImports.value) }}>
+                        {formatUSDvalue(hoveredCountry.totalImports.value)}
+                        </div>
+                        <div className='annotate'><div className='text'>{formatUSDorder(hoveredCountry.totalImports.value)}</div></div>
+                        <span className='money-label'>Imports</span>
                     </div>
-                    <div className='annotate'><div className='text'>{formatUSDorder(hoveredCountry.totalImports.value)}</div></div>
-                    <span className='money-label'>Imports</span>
-                </div>
 
-                <div className='circle-wrapper'>
-                    <div className="circle" style={{ backgroundColor: getDemocracyColor(hoveredCountry.democracyIndex.value) }}>
-                    {hoveredCountry.democracyIndex.value}
+                    <div className='circle-wrapper'>
+                        <div className="circle" style={{ backgroundColor: getDemocracyColor(hoveredCountry.democracyIndex.value) }}>
+                        {hoveredCountry.democracyIndex.value}
+                        </div>
+                        <span className='circle-label'>Democracy Index<sup>[1]</sup></span>
                     </div>
-                    <span className='circle-label'>Democracy Index<sup>[1]</sup></span>
-                </div>
 
-                <div className='circle-wrapper'>
-                    <div className="circle" style={{ backgroundColor: getPeaceColor(hoveredCountry.peaceIndex.value) }}>
-                    {hoveredCountry.peaceIndex.value}
+                    <div className='circle-wrapper'>
+                        <div className="circle" style={{ backgroundColor: getPeaceColor(hoveredCountry.peaceIndex.value) }}>
+                        {hoveredCountry.peaceIndex.value}
+                        </div>
+                        <span className='circle-label'>Peace Index <sup>[2]</sup></span>
                     </div>
-                    <span className='circle-label'>Peace Index <sup>[2]</sup></span>
+                    
+                    </div>
                 </div>
-                
-                </div>
+                }
             </div>
     )
 }
