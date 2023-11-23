c3ce433517eb90dd778e17c53c8bb39b8a6a04e3
Frederik Laubisch <kafkaese@gmail.com>
2023-11-23 13:35:15 +0100

    feat: Add Impressum component

---
+++ src/components/Impressum.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Impressum = () => {
+    return (
+        <div>
+            <h1>Impressum</h1>
+            <p>Frederik Laubisch</p>
+            <p>Allerstr. 43</p>
+            <p>12049 Berlin</p>
+            <p>Germany</p>
+            <p></p>
+            <p><a href="mailto:f.laubisch@posteo.de">f.laubisch@posteo.de</a></p>
+            <p><a href="github.com/Kafkaese">Github</a></p>
+        </div>
+    )
+}
+
+export default Impressum;
