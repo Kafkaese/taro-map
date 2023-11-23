1e36d72cc9fd6b86cba6d4b4629c0e235078df02
Frederik Laubisch <kafkaese@gmail.com>
2023-11-23 15:11:21 +0100

    feat: Finalize and style Impressum

--- src/components/Impressum.jsx
+++ src/components/Impressum.jsx
@@ -4,13 +4,17 @@ const Impressum = () => {
     return (
         <div className="impressum">
             <h1 style={{textAlign: 'center'}}>Impressum</h1>
-            <p>Frederik Laubisch</p>
-            <p>Allerstr. 43</p>
-            <p>12049 Berlin</p>
-            <p>Germany</p>
-            <p></p>
-            <p><a href="mailto:f.laubisch@posteo.de">f.laubisch@posteo.de</a></p>
-            <p><a href="github.com/Kafkaese">Github</a></p>
+            <div style={{marginLeft: '100px', marginRight: '100px'}}>
+                <p>Frederik Laubisch</p>
+                <p>Allerstr. 43</p>
+                <p>12049 Berlin</p>
+                <p>Germany</p>
+                <br/>
+                <p><a style={{color: 'white'}} href="mailto:f.laubisch@posteo.de">f.laubisch@posteo.de</a></p>
+                <p><a style={{color: 'white'}} href="github.com/Kafkaese">github.com/Kafkaese</a></p>
+                <br/>
+                <p>Responsible for the content in accordance with  §18 Abs. 2 MStV: Frederik Laubisch</p>
+            </div>
         </div>
     )
 }
