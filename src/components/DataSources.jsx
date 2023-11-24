df531ec7a6432612a9a95285f4499d41abea4c14
Frederik Laubisch <kafkaese@gmail.com>
2023-11-24 13:34:32 +0100

    feat: Make popup width dependant on content; use for data source poup

--- src/components/DataSources.jsx
+++ src/components/DataSources.jsx
@@ -4,7 +4,28 @@ const DataSources = () => {
 
     return (
         <div>
-            <h1>Data Sources</h1>
+            <h1 style={{textAlign: 'center', color: 'white'}}>Data Sources</h1>
+            <div style={{ display: 'table', clear: 'both', marginLeft: '50px', marginRight: '50px', color: 'white', fontSize: '11pt'}}>
+                <div style={{float: 'left', width: '50%'}}>
+                    <p>The data displayed here regarding the import and export of arms is 
+                    directly sourced from the <a href="https://caat.org.uk/" style={{color: '#06d3fc'}}>CAAT</a> github 
+                    respository (accessible <a href="https://github.com/caatdata/eu-arms-export-data" style={{color: '#06d3fc'}}>here</a>). 
+                    The raw data as provided by CAAT is sourced from the Official Journal of the European Union 
+                    annual reports on the European Union Code of Conduct on Arms Exports, published by The 
+                    European Union Council Working Party on Conventional Arms Exports. Re-use and -publication
+                    is explicitly permited for non-commercial use by the 
+                    <a href="https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=CELEX:32011D0833" style={{color: '#06d3fc'}}> Commission Decision
+                    of 12 December 2011 on the reuse of Commission documents</a>.</p>
+                    <p>As the data contains only numbers about arms exports and only of some bu not all members of the European Union,
+                    all data displayed regarding arms imports, in particular for non-EU countries, is based on aggredated data from EU member states. Therefore the data is to be considered highly incomplete.</p>
+                    <p>I make no claims of nor take any responsibility for correctness and/or completeness of the data presented here.</p>
+                </div>
+                <div style={{float: 'left', width: '50%'}}>
+                    <p>
+                        lorem ipsum yadada
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -8,12 +8,17 @@ import DataSources from './DataSources';
  * 
  * @param {string} content Type of content to be displayed. ust be one of [impressum, data]
  * @param {function} setShowPopUp Function that controlled wether the popup is enabled. Passed from App.
- * 
+ *
  */
 const PopUp = (content, setShowPopUp) => {
 
+    let boxWidth = '500px'
+    let boxLeftMargin = '250px'
+
+    if (content.content === 'data') {boxWidth = '1000px'; boxLeftMargin = '-500px'}
+
     return (
-        <div className='popup-container'>
+        <div className='popup-container' style={{width: boxWidth, marginLeft: boxLeftMargin}}>
             <div>{setShowPopUp.content}</div>
             <button style={{ position: 'absolute', top: '5px', right: '5px', color: 'white', backgroundColor: '#374151', border: 'none', borderRadius: '10px', textAlign: 'center' }} onClick={() => { content.setShowPopUp('none') }}>X</button>
             <div>
