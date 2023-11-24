c422e4638a21e241820f2e039e64ce7071e32fbd
Frederik Laubisch <kafkaese@gmail.com>
2023-11-24 11:33:29 +0100

    feat: Style and place close button

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -5,19 +5,19 @@ import Impressum from './Impressum';
 const PopUp = (content, setShowPopUp) => {
 
 
-return (
-    <div className='popup-container'>
-        <div>{setShowPopUp.content}</div>
-        <button onClick={() => {content.setShowPopUp('None')}}>x</button>
-        <div>
-            {{
-            'impressum': <Impressum/>,
-            'data': <Impressum className='impressum'/>
-            }[content.content]}
+    return (
+        <div className='popup-container'>
+            <div>{setShowPopUp.content}</div>
+            <button style={{ position: 'absolute', top: '5px', right: '5px', color: 'white', backgroundColor: '#374151', border: 'none', borderRadius: '10px', textAlign: 'center' }} onClick={() => { content.setShowPopUp('None') }}>X</button>
+            <div>
+                {{
+                    'impressum': <Impressum />,
+                    'data': <Impressum className='impressum' />
+                }[content.content]}
+            </div>
         </div>
-    </div>
-)
-    
+    )
+
 }
 
 
